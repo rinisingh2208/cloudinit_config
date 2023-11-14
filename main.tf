de257779aac611bfa0ae9b847056800bbff93ac7
rinisingh2208 <rini2208@gmail.com>
2023-11-15 01:00:34 +0530

    Update main.tf
    main.tf to apply userdata.tpl to run shell script ec2 instances

--- main.tf
+++ main.tf
@@ -6,12 +6,30 @@ data "cloudinit_config" "node_group" {
     content_type = "text/x-shellscript"
     content      = file("${path.module}/userdata.tpl")
   }
-  part {
-    content_type = "text/x-shellscript"
-    content      = file("${path.module}/updatingdockerconfig.sh")
-  }
-  part {
-    content_type = "text/x-shellscript"
-    content      = file("${path.module}/customsysctl.sh")
-  }
+
 }
+
+resource "aws_launch_template" "eks_launch_template" {
+
+  block_device_mappings {
+    device_name = "/dev/xvda"
+
+    ebs {
+      volume_size = var.node_disk_size
+    }
+  }
+  key_name = var.ec2_ssh_key
+  network_interfaces {
+    associate_public_ip_address = false
+
+  }
+
+  user_data = base64encode(data.cloudinit_config.node_group.rendered)
+
+  tag_specifications {
+    resource_type = "instance"
+
+    tags = {
+      Name = "EKS-MANAGED-NODE"
+    }
+  }
